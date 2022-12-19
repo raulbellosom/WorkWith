91b6af5d627313cdf68f4d362b92a7a6b3fb9bda
Raul Belloso M <raul.belloso.m@gmail.com>
2022-12-18 21:19:36 -0600

    adding enviroment

--- server/config.js
+++ server/config.js
@@ -1,7 +1,9 @@
-export const PORT = 4000;
+import { config } from "dotenv";
+config();
 
-export const DB_HOST = "localhost";
-export const DB_PORT = 3306;
-export const DB_USER = "root";
-export const DB_NAME = "workwithdb";
-export const DB_PASS = "racoon";
+export const PORT = process.env.PORT || 4000;
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_USER = process.env.DB_NAME || "root";
+export const DB_NAME = process.env.DB_USER || "workwithdb";
+export const DB_PASS = process.env.DB_PASS || "racoon";
